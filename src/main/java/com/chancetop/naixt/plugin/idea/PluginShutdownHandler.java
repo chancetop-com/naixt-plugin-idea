396ef6d88f55fd40ac917555ac4619d5c0037100
Stephen <stephenzeng@chancetop.com>
2025-02-25 17:38:40 +0800

    update version and not throw in stop

--- src/main/java/com/chancetop/naixt/plugin/idea/PluginShutdownHandler.java
+++ src/main/java/com/chancetop/naixt/plugin/idea/PluginShutdownHandler.java
@@ -3,6 +3,8 @@ package com.chancetop.naixt.plugin.idea;
 import com.chancetop.naixt.plugin.idea.server.AgentServiceManagementService;
 import com.intellij.ide.AppLifecycleListener;
 import com.intellij.openapi.application.ApplicationManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -10,6 +12,7 @@ import java.io.IOException;
  * @author stephen
  */
 public class PluginShutdownHandler implements AppLifecycleListener {
+    private final Logger logger = LoggerFactory.getLogger(PluginShutdownHandler.class);
     public PluginShutdownHandler() {
         ApplicationManager.getApplication().getMessageBus().connect().subscribe(AppLifecycleListener.TOPIC, this);
     }
@@ -19,7 +22,7 @@ public class PluginShutdownHandler implements AppLifecycleListener {
         try {
             ApplicationManager.getApplication().getService(AgentServiceManagementService.class).stop();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            logger.error("failed to stop agent service", e);
         }
     }
 }
